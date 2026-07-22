622f450f76d9e6f655a682fd67620f210cb35672
moataz13-amd <202401348@pua.edu.eg>
2026-07-22 18:23:28 +0300

    use [...slug].js catch-all for all /api routes on Vercel

--- api/[...slug].js
+++ api/[...slug].js
@@ -2,16 +2,7 @@ try {
     require('dotenv').config()
 } catch (e) {}
 
-let app
-try {
-    const mod = require('../backend/app')
-    app = mod.createApp ? mod.createApp() : mod
-} catch (err) {
-    const express = require('express')
-    app = express()
-    app.get('*', (req, res) => {
-        res.status(500).json({ error: 'Failed to load app', message: err.message, stack: err.stack })
-    })
-}
+const { createApp } = require('../backend/app')
+const app = createApp()
 
 module.exports = app
