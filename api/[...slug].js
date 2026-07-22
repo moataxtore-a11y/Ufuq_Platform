8aa79b4e7a198833479c17dd8889489a98abdff0
moataz13-amd <202401348@pua.edu.eg>
2026-07-22 18:30:05 +0300

    add functions config + api/package.json for Vercel deps

--- api/[...slug].js
+++ api/[...slug].js
@@ -2,7 +2,17 @@ try {
     require('dotenv').config()
 } catch (e) {}
 
-const { createApp } = require('../backend/app')
-const app = createApp()
+let handler
+try {
+    const { createApp } = require('../backend/app')
+    const app = createApp()
+    handler = app
+} catch (err) {
+    const express = require('express')
+    handler = express()
+    handler.all('*', (req, res) => {
+        res.status(500).json({ error: err.message })
+    })
+}
 
-module.exports = app
+module.exports = handler
