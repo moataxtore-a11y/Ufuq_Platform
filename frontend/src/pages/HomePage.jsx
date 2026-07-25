a6732287de8480ab5c5ecef338544db716019f78
moataz13-amd <202401348@pua.edu.eg>
2026-07-26 01:01:58 +0300

    update home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,5 @@
 import SiteLayout from '../components/layout/SiteLayout.jsx'
 import HeroSection from '../components/home/HeroSection.jsx'
-import AboutSection from '../components/home/AboutSection.jsx'
-import ServicesSection from '../components/home/ServicesSection.jsx'
 import SubjectsSection from '../components/home/SubjectsSection.jsx'
 import PartnersSection from '../components/home/PartnersSection.jsx'
 import FeaturedTeachersCoursesSection from '../components/home/FeaturedTeachersCoursesSection.jsx'
@@ -12,11 +10,9 @@ export default function HomePage() {
   return (
     <SiteLayout>
       <HeroSection />
-      <AboutSection />
-      <ServicesSection />
-      <SubjectsSection />
       <ChooseTeachersSection />
       <FeaturedTeachersCoursesSection />
+      <SubjectsSection />
       <PartnersSection />
       <LandingCtaSection />
     </SiteLayout>
